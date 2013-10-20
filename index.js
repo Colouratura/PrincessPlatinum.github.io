6e6a87f7b4010eb42983cd354a6dcf9689716a60
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-20 16:33:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-(function () {
-    var cursor,
-        typer = function (id) {
-            return document.getElementById(id);
-        },
-        nl2br = function (txt) {
-            return txt.replace(/\n/g, "<br />");
-        },
-        writeit = function (from, e) {
-            e = e || window.event;
-            var w = typer("writer");
-            var tw = from.value;
-            w.innerHTML = nl2br(tw);
-        },
-        moveIt = function (count, e) {
-            e = e || window.event;
-            var keycode = e.keyCode || e.which;
-            if (keycode == 37 && parseInt(cursor.style.left) >= (0 - ((count - 1) * 10))) {
-                cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
-            } else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
-                cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
-            }
+var cursor,
+    typer = function (id) {
+        return document.getElementById(id);
+    },
+    nl2br = function (txt) {
+        return txt.replace(/\n/g, "<br />");
+    },
+    writeit = function (from, e) {
+        e = e || window.event;
+        var w = typer("writer");
+        var tw = from.value;
+        w.innerHTML = nl2br(tw);
+    },
+    moveIt = function (count, e) {
+        e = e || window.event;
+        var keycode = e.keyCode || e.which;
+        if (keycode == 37 && parseInt(cursor.style.left) >= (0 - ((count - 1) * 10))) {
+            cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+        } else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
+            cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+        }
 
-        };
-
-    window.onload = function () {
-        cursor = typer("cursor");
-        cursor.style.left = "0px";
     };
-});
+
+window.onload = function () {
+    cursor = typer("cursor");
+    cursor.style.left = "0px";
+};
 
 (function ($) {
     if (typeof (Storage) !== "undefined") {
