251d7be3c213eb1f8e555df1330f58506471945b
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-20 14:53:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-/* Created by Wikia user Princess Platinum */
-function siggify() {
-	//Get the value of all input boxes and dropdowns
-	var font = document.getElementById('font-selector').value || '',
-		userName = document.getElementById('username').value || '',
-		userText = document.getElementById('username-text').value || '',
-		userColor = document.getElementById('username-color').value || '',
-		talkText = document.getElementById('talkpage-text').value || '',
-		talkColor = document.getElementById('talkpage-color').value || '',
-		borderType = document.getElementById('border-type').value || '',
-		borderWidth = document.getElementById('width-selector').value || '',
-		backgroundColor = document.getElementById('background-color').value || '',
-		borderColor = document.getElementById('border-color').value || '',
-		topLeft = document.getElementById('top-left').value || '0',
-		topRight = document.getElementById('top-right').value || '0',
-		bottomRight = document.getElementById('bottom-right').value || '0',
-		bottomLeft = document.getElementById('bottom-left').value || '0';
+var cursor,
+    $ = function (id) {
+        return document.getElementById(id);
+    },
+    nl2br = function (txt) {
+        return txt.replace(/\n/g, "<br />");
+    },
+    writeit = function (from, e) {
+        e = e || window.event;
+        var w = $("writer");
+        var tw = from.value;
+        w.innerHTML = nl2br(tw);
+    },
+    moveIt = function (count, e) {
+        e = e || window.event;
+        var keycode = e.keyCode || e.which;
+        if (keycode == 37 && parseInt(cursor.style.left) >= (0 - ((count - 1) * 10))) {
+            cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+        } else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
+            cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+        }
 
-	//Put the signature together
-	var borderRadius = '-moz-border-radius: ' + topLeft +'em ' + topRight + 'em ' + bottomRight + 'em ' + bottomLeft + 'em; -webkit-border-radius: ' + topLeft + 'em ' + topRight + 'em ' + bottomRight + 'em ' + bottomLeft + 'em; border-radius: ' + topLeft + 'em ' + topRight + 'em ' + bottomRight + 'em ' + bottomLeft + 'em;',
-		sigNature = '<span style="padding: 0px 5px 0px 5px; border: ' + borderType + ' ' + borderWidth + 'px ' + borderColor + '; background: ' + backgroundColor + '; ' + borderRadius + '">[[User:' + userName + '|<span style="color: ' + userColor + '; font: ' + font + ';">' + userText + '</span>]]&nbsp;[[User talk:' + userName + '|<span style="color: ' + talkColor + '; font: ' + font +';">' + talkText + '</span>]]</span>';
+    };
 
-	//Print code to output box
-	document.getElementById( 'code-output' ).value = sigNature;
-}
+window.onload = function () {
+    cursor = $("cursor");
+    cursor.style.left = "0px";
+};
